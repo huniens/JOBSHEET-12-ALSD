4f548ab4257e3a8e1440b09456378ce8b5903d84
huniens <144862617+huniens@users.noreply.github.com>
2024-05-28 00:46:00 +0700

    Add files via upload

--- DoubleLinkedLists11.java
+++ DoubleLinkedLists11.java
@@ -15,9 +15,9 @@ public class DoubleLinkedLists11 {
         if (isEmpty()) {
             head = new Node11(null, item, null);
         } else {
-            Node11 newNode = new Node11(null, item, head);
-            head.prev = newNode;
-            head = newNode;
+            Node11 newNode11 = new Node11(null, item, head);
+            head.prev = newNode11;
+            head = newNode11;
         }
         size++;
     }
@@ -30,8 +30,8 @@ public class DoubleLinkedLists11 {
             while (current.next != null) {
                 current = current.next;
             }
-            Node11 newNode = new Node11(current, item, null);
-            current.next = newNode;
+            Node11 newNode11 = new Node11(current, item, null);
+            current.next = newNode11;
             size++;
         }
     }
@@ -49,15 +49,14 @@ public class DoubleLinkedLists11 {
                 i++;
             }
             if (current.prev == null) {
-                Node11 newNode = new Node11(null, item, current);
-                current.prev = newNode;
-                head = newNode;
+                Node11 newNode11 = new Node11(null, item, current);
+                current.prev = newNode11;
+                head = newNode11;
             } else {
-                Node11 newNode = new Node11(current.prev, item, current);
-                newNode.prev = current.prev;
-                newNode.next = current;
-                newNode.prev.next = newNode;
-                current.prev = newNode;
+                Node11 newNode11 = new Node11(current.prev, item, current);
+                newNode11.prev.next = newNode11;
+                newNode11.next = current;
+                current.prev = newNode11;
             }
         }
         size++;
@@ -82,6 +81,61 @@ public class DoubleLinkedLists11 {
             System.out.println("\nberhasil diisi");
         } else {
             System.out.println("Linked Lists Kosong");
+        }
+    }
+
+    public void removeFirst() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, tidak dapat dihapus!");
+        } else if (size == 1) {
+            head = null;
+            size--;
+        } else {
+            head = head.next;
+            head.prev = null;
+            size--;
+        }
+    }
+
+    public void removeLast() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Linked list masih kosong, tidak dapat dihapus!");
+        } else if (head.next == null) {
+            head = null;
+            size--;
+            return;
+        }
+        Node11 current = head;
+        while (current.next.next != null) {
+            current = current.next;
+        }
+        current.next = null;
+        size--;
+    }
+
+    public void remove(int index) throws Exception {
+        if (isEmpty() || index >= size) {
+            throw new Exception("Nilai indeks di luar batas");
+        } else if (index == 0) {
+            removeFirst();
+        } else {
+            Node11 current = head;
+            int i = 0;
+            while (i < index) {
+                current = current.next;
+                i++;
+            }
+            if (current.next == null) {
+                current.prev.next = null;
+            } else if (current.prev == null) {
+                current = current.next;
+                current.prev = null;
+                head = current;
+            } else {
+                current.prev.next = current.next;
+                current.next.prev = current.prev;
+            }
+            size--;
         }
     }
 }
--- Node11.java
+++ Node11.java
@@ -1,12 +1,10 @@
-/**
- * Node11
- */
 public class Node11 {
-        int data;
-        Node11 prev, next;
-        Node11(Node11 prev, int data, Node11 next) {
+    int data;
+    Node11 prev, next;
+
+    Node11(Node11 prev, int data, Node11 next) {
         this.prev = prev;
         this.data = data;
         this.next = next;
-        }
+    }
 }
